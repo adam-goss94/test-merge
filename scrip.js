c2b660f29b31f93997abdcfc4906e0f31d6def09
Adam Goss <adam.goss94@gmail.com>
2020-04-08 20:50:07 +0200

    init

---
+++ scrip.js
@@ -0,0 +1,16 @@
+const test = "test";
+console.log(test);
+
+const obj = {
+    id: 'first',
+    img: "img",
+    src: "src",
+    alt: "asdasd",
+}
+
+const jbo = {
+    id: 'first',
+    img: "img",
+    src: "src",
+    alt: "asdasd",
+}
